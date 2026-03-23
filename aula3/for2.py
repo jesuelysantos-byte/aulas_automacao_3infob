a0064b5d6e1b00f8ccd73590e921d4a0003c3904
Jesuely Vitória <jesuely.santos@alunos.ifsuldeminas.edu.br>
2026-03-23 08:45:25 -0300

    Aula 3 - Estrutura de Repetição

---
+++ aula3/for2.py
@@ -0,0 +1,15 @@
+#Estrutura de repetição For:
+#Percorrer listas
+#Lista estrutura de dados composta (armazena diversos valores)
+#Variável que possui vários valores
+
+nomes = ['Jesuely', 'Mariana', 'Eloah', 'Ivan']
+print(nomes)
+
+#Imprime todos os nomes da lista utilizando range
+for i in range(4):
+    print(nomes[i])
+
+#Imprime todos os nomes usando interador de lista
+for nome in nomes:
+    print (nome)
